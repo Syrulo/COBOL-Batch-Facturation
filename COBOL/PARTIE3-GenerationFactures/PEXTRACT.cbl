dc041dd171e2d90344a55d68ef2b20100c41fb19
Thomas Borestel <148350381+Syrulo@users.noreply.github.com>
2026-01-22 13:43:46 +0100

    Update PEXTRACT.cbl

--- COBOL/PARTIE3-GenerationFactures/PEXTRACT.cbl
+++ COBOL/PARTIE3-GenerationFactures/PEXTRACT.cbl
@@ -19,7 +19,7 @@
 *> Remarques :
 *> - Les données sont placées dans ENREXTRACT
 *> - Un sous-programme gère la date en toutes lettres si nécessaire
-*> - Le code est fourni à titre pédagogique
+*> - code est fourni à titre pédagogique
 *> ==========================================================
 
        ID DIVISION.                                                     
