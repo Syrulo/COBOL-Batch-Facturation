05a3ea0a763e9747e3654592c896ee06fdb7067c
Thomas Borestel <148350381+Syrulo@users.noreply.github.com>
2026-01-22 10:34:40 +0100

    Create PNEWPROD.cbl

---
+++ COBOL/PNEWPROD.cbl
@@ -0,0 +1,6 @@
+*> ==========================================================
+*> PNEWPROD.cbl - Programme principal
+*> Traitement des nouveaux produits et insertion en DB2
+*> Académique, formation COBOL / Mainframe
+*> Appelle les sous-programmes : PFORMAT, PDEVISE
+*> ==========================================================
