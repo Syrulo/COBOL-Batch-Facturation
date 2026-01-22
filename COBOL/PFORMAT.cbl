3ab198f5f2e1db0aec101d16996d594803d1a6a1
Thomas Borestel <148350381+Syrulo@users.noreply.github.com>
2026-01-22 10:43:33 +0100

    Create PFORMAT.cbl

---
+++ COBOL/PFORMAT.cbl
@@ -0,0 +1,49 @@
+*> ==========================================================
+*> PFORMAT.cbl - Sous-programme de formatage des descriptions
+*> Objectif : mettre une majuscule au début de chaque mot
+*> et le reste en minuscules
+*> Entrée : DESCRIPTION (PIC X(30))
+*> Sortie : DESCRIPTION (modifiée)
+*> ==========================================================
+       ID DIVISION.                                                     
+       PROGRAM-ID. PFORMAT.                                             
+                                                                        
+       ENVIRONMENT DIVISION.                                            
+       CONFIGURATION SECTION.                                           
+       SPECIAL-NAMES.                                                   
+           DECIMAL-POINT IS COMMA.                                      
+                                                                        
+       DATA DIVISION.                                                   
+       FILE SECTION.                                                    
+                                                                        
+       WORKING-STORAGE SECTION.                                         
+       01  CPT           PIC 99 VALUE 1.                                
+       01  WS-RESULT     PIC X(30).                                     
+       01  WS-SPACE      PIC X VALUE 'Y'.                               
+                                                                        
+       LINKAGE SECTION.                                                 
+       01  LS-STRING     PIC X(30).                                     
+                                                                        
+       PROCEDURE DIVISION USING LS-STRING.                              
+                                                                        
+           PERFORM VARYING CPT FROM 1 BY 1 UNTIL CPT >                  
+                   FUNCTION LENGTH(LS-STRING)                           
+              IF WS-SPACE = 'Y'                                         
+                 MOVE FUNCTION UPPER-CASE(LS-STRING(CPT:1))             
+                 TO WS-RESULT(CPT:1)                                    
+              ELSE                                                      
+                 MOVE FUNCTION LOWER-CASE(LS-STRING(CPT:1))             
+                 TO WS-RESULT(CPT:1)                                    
+              END-IF                                                    
+              IF LS-STRING(CPT:1) = SPACE                               
+                 MOVE 'Y' TO WS-SPACE                                   
+              ELSE                                                      
+                 MOVE 'N' TO WS-SPACE                                   
+              END-IF                                                    
+           END-PERFORM                                                  
+                                                                        
+           MOVE WS-RESULT TO LS-STRING                                  
+                                                                        
+           MOVE 'Y' TO WS-SPACE                                         
+                                                                        
+           GOBACK.                                                      
