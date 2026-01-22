d5e5d9419a56493605647d375c66f37d057074d4
Thomas Borestel <148350381+Syrulo@users.noreply.github.com>
2026-01-22 12:50:38 +0100

    Update PNEWPROD.cbl

--- COBOL/PARTIE1-AjoutNouveauxProduits/PNEWPROD.cbl
+++ COBOL/PARTIE1-AjoutNouveauxProduits/PNEWPROD.cbl
@@ -69,7 +69,7 @@
            GOBACK.                                                      
                                                                         
        1000-DEBUT.                                                      
-                                                                        
+*> Ouverture du fichier des nouveaux produits (DDNEWPRO)                                                                        
               OPEN INPUT  FNEWPRO                                       
                                                                         
               READ FNEWPRO INTO WS-ENRPRO                               
@@ -77,31 +77,31 @@
                        MOVE 'Y' TO EOF                                  
               END-READ                                                  
               .                                                         
-                                                                        
+*> Lecture initiale du premier enregistrement pour démarrer la boucle                                                                        
        2000-TRAITEMENT.                                                 
-                                                                        
+*> Décomposition de l’enregistrement CSV en champs internes                                                                        
               UNSTRING WS-ENRPRO DELIMITED BY ';'                       
                     INTO P-NO DESCRIPTION PRICE DEVISE                  
               END-UNSTRING.                                             
                                                                         
               MOVE P-NO TO P-P-NO                                       
-                                                                        
+*> Décomposition de l’enregistrement CSV en champs internes                                                                       
               CALL PFORMAT USING DESCRIPTION                            
                                                                         
               MOVE DESCRIPTION TO P-DESCRIPTION-TEXT                    
               MOVE LENGTH OF P-DESCRIPTION-TEXT TO P-DESCRIPTION-LEN    
-                                                                        
+*> Conversion du prix selon la devise avec PDEVISE                                                                        
               CALL PDEVISE USING DEVISE PRICE WS-RESULT                 
                                                                         
               MOVE WS-RESULT TO P-PRICE                                 
-                                                                        
+*> Insertion du produit dans la table DB2 PRODUCTS                                                                       
               EXEC SQL                                                  
                   INSERT INTO API12.PRODUCTS                            
                       (P_NO, DESCRIPTION, PRICE)                        
                   VALUES                                                
                       (:P-P-NO, :P-DESCRIPTION, :P-PRICE)               
               END-EXEC                                                  
-                                                                        
+*> Vérification du résultat de l’insertion SQL                                                                      
               IF SQLCODE NOT = 0                                        
                 DISPLAY 'ERREUR INSERT : ' SQLCODE                      
                 DISPLAY 'PRODUIT ' P-NO ' NON INSERE.'                  
@@ -115,7 +115,8 @@
               END-READ                                                  
               .                                                         
                                                                         
-       3000-FIN.                                                        
+       3000-FIN.
+*> Fermeture du fichier des produits
               CLOSE FNEWPRO                                             
               .                                                         
                                 
